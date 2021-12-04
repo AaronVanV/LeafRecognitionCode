4643afbe0a69ffaafa42984110ff95fb83d10dd9
Aaron <aaron.van.vyve@telenet.be>
2021-12-04 15:59:25 +0100

    added prediction

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LeafRecognitionCode
+
 Deep Learning code for Leaf Recognition using keras on python and matlab
+
+## INSTALL for learning and predict
+
+To install the packages needed for training and predictions, you can execute the following command:
+
+```cmd
+pip install -r .\requirements.txt
+```
+
+or you can look at the requirements.txt file and manually install them separately:
+
+```cmd
+pip install keras
+pip install matplotlib
+...
+```
---
+++ predict.py
@@ -0,0 +1,56 @@
+# load_model_sample.py
+#from PIL.Image import ImagePointHandler
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+import numpy as np
+
+
+
+
+# load model
+model = load_model("model.h5")
+
+img_width=324
+img_height=324
+
+# load a single image
+
+
+inputdataGen = ImageDataGenerator()
+
+input = inputdataGen.flow_from_directory("data/img",target_size=(img_width, img_height),batch_size=1, class_mode=None)
+
+
+
+print()
+
+    
+# check prediction
+    
+pred = model.predict(input)
+
+print(pred)
+
+arr=pred
+number=0
+
+for leaf in arr:
+    number+=1
+    
+    maxElement = np.amax(leaf)
+    certain=str(maxElement.item())
+    index = np.where(leaf == maxElement)[0]
+
+    indextext= index[0].item()+1
+
+    print("Number: "+ str(number) + ", Certain: "+ certain+ ", Guess: Leaf "+ str(indextext))
+
+
+
+
+
+
+
